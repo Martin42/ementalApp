2be4a89d006022b8377fd0da259405ed1b1632b5
Mclara1 <mclara1@ua.pt>
2021-05-26 16:11:41 +0100

    Info3 e texto do Consentimento

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Login from './Screens/Login';
 import Botao from './Screens/Botao';
 import Info from './Screens/Registo/Info';
 import Info2 from './Screens/Registo/Info2';
+import Info3 from './Screens/Registo/Info3';
 import Consentimento from './Screens/Registo/Consentimento';
 import Pedido from './Screens/Registo/Pedido';
 
@@ -25,6 +26,7 @@ export default function App() {
               <Stack.Screen name='Registo' component={Selecionar} />
               <Stack.Screen name='Info' component={Info} /> 
               <Stack.Screen name='Info2' component={Info2} />
+              <Stack.Screen name='Info3' component={Info3} />
               <Stack.Screen name='Consentimento' component={Consentimento} />
               <Stack.Screen name='Pedido' component={Pedido} />
           </Stack.Navigator>
