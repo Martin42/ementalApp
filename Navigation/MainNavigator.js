69bfdfa1d590b6290d872fd6f91a0cf32a49e659
Xunga <36819968+ItsXunga@users.noreply.github.com>
2021-06-07 18:53:31 +0100

    here it goes

--- Navigation/MainNavigator.js
+++ Navigation/MainNavigator.js
@@ -10,6 +10,9 @@ import Selecionar from '../Screens/Registo/Selecionar';
 import Info from '../Screens/Registo/Info';
 import Info2 from '../Screens/Registo/Info2';
 import Info3 from '../Screens/Registo/Info3';
+import Info4 from '../Screens/Registo/Info4';
+import Info5 from '../Screens/Registo/Info5';
+import Info6 from '../Screens/Registo/Info6';
 import Consentimento from '../Screens/Registo/Consentimento';
 import Pedido from '../Screens/Registo/Pedido';
 import Registo from '../Screens/Registo/Registo';
@@ -20,6 +23,7 @@ import QiSaraD from '../Screens/App/Sara/QiSaraD';
 import QiSaraE from '../Screens/App/Sara/QiSaraE';
 import QiSaraF from '../Screens/App/Sara/QiSaraF';
 import Homepage from '../Screens/App/Homepage';
+import Ep1_Sara from '../Screens/App/Sara/Ep1_Sara';
 
 
 
@@ -41,6 +45,9 @@ const MainStackNavigator = () => {
                 <Stack.Screen name='Info' component={Info} />
                 <Stack.Screen name='Info2' component={Info2} />
                 <Stack.Screen name='Info3' component={Info3} />
+                <Stack.Screen name='Info4' component={Info4} />
+                <Stack.Screen name='Info5' component={Info5} />
+                <Stack.Screen name='Info6' component={Info6} />
                 <Stack.Screen name='Consentimento' component={Consentimento} />
                 <Stack.Screen name='Pedido' component={Pedido} />
                 <Stack.Screen name='Registo' component={Registo} />
@@ -51,6 +58,7 @@ const MainStackNavigator = () => {
                 <Stack.Screen name='QiSaraD' component={QiSaraD} />
                 <Stack.Screen name='QiSaraE' component={QiSaraE} />
                 <Stack.Screen name='QiSaraF' component={QiSaraF} />
+                <Stack.Screen name='Ep1_Sara' component={Ep1_Sara} />
                 
         
             </Stack.Navigator>
